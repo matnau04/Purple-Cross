dda1be56d3273a58ed14e56fd40a4f0c4a276fc3
matnau04 <matteo.naudi@gmail.com>
2026-06-29 12:13:26 +0200

    Add employee status labels

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,40 @@
 import employees from './data/employees.json';
 
 const employeeCount = employees.length;
+
+const dateFormatter = new Intl.DateTimeFormat('en-GB', {
+  day: '2-digit',
+  month: 'short',
+  year: 'numeric',
+});
+
+const getDateStatus = (dateValue, futureLabel, pastLabel) => {
+  if (!dateValue) {
+    return null;
+  }
+
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+
+  const targetDate = new Date(dateValue);
+  targetDate.setHours(0, 0, 0, 0);
+
+  return targetDate > today ? futureLabel : pastLabel;
+};
+
+const getEmploymentStatus = (dateValue) =>
+  getDateStatus(dateValue, 'Employed soon', 'Currently employed');
+
+const getTerminationStatus = (dateValue) =>
+  getDateStatus(dateValue, 'To be terminated', 'Terminated');
+
+const formatDate = (dateValue) => {
+  if (!dateValue) {
+    return 'Not scheduled';
+  }
+
+  return dateFormatter.format(new Date(dateValue));
+};
 </script>
 
 <template>
@@ -49,8 +83,21 @@ const employeeCount = employees.length;
               <td>{{ employee.fullName }}</td>
               <td>{{ employee.occupation }}</td>
               <td>{{ employee.department }}</td>
-              <td>{{ employee.dateOfEmployment }}</td>
-              <td>{{ employee.terminationDate || 'Active' }}</td>
+              <td>
+                <span>{{ formatDate(employee.dateOfEmployment) }}</span>
+                <span class="status-pill status-pill--employment">
+                  {{ getEmploymentStatus(employee.dateOfEmployment) }}
+                </span>
+              </td>
+              <td>
+                <span>{{ formatDate(employee.terminationDate) }}</span>
+                <span
+                  v-if="getTerminationStatus(employee.terminationDate)"
+                  class="status-pill status-pill--termination"
+                >
+                  {{ getTerminationStatus(employee.terminationDate) }}
+                </span>
+              </td>
             </tr>
           </tbody>
         </table>
